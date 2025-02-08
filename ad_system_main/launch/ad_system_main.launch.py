c8cb9aa1cbf86efd13d12f24fc0fac552047f97f
Jia Yong Lau <Jia-Yong.Lau@stud.hs-coburg.de>
2025-02-08 17:26:47 +0100

    Update ad_system_main/launch/ad_system_main.launch.py

--- ad_system_main/launch/ad_system_main.launch.py
+++ ad_system_main/launch/ad_system_main.launch.py
@@ -15,20 +15,25 @@ def generate_launch_description():
     #     default_value='a',
     #     description='Task parameter to set the task'
     # )
-    package_names = ["interior_monitoring","lane_detection","localization","v2x_receiver","environment_model","global_planning","behavior_planning","local_planning","lateral_control","longitudinal_control","v2x_transmitter"]
+    package_names = ["interior_monitoring","lane_detection","localization_pkg","v2x_receiver","environment_model","global_planning","behavior_planning","local_planning","lateral_control","longitudinal_control","v2x_transmitter"]
+    node_names = ["interior_monitoring_node","lane_detection_node","localization_node","v2x_receiver_node","environment_node","global_planner","behavior_planner","local_planner","lateral_control","longitudinal_control","v2x_transmitter_node"]
     nodes = []
     handlers = []
 
-    for package in package_names:
+    if len(package_names) != len(node_names):
+        print("Error: The lengths of package_names and node_names are not the same!")
+        sys.exit(1)
+
+    for i in range(len(package_names)):
         restart_counter = f"{package}_restarts"
         SetLaunchConfiguration(restart_counter, "0")
         # Declare a launch configuration to track restarts
         nodes.append(DeclareLaunchArgument(restart_counter, default_value="0"))
 
         node_action = Node(
-            package=package,
-            executable=package,
-            name=package
+            package=package_names[i],
+            executable=node_names[i],
+            name=node_names[i]
         )
 
         # Restart if node does not launch successfully
